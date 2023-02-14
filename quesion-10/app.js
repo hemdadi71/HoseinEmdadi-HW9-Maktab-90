cf769bc0dffabba1e83b7cfa9dc0dfac64bdedf1
Hosein Emdadi <hemdadi71@gmail.com>
2023-02-14 16:12:29 +0330

    js codes not compeleted

--- quesion-10/app.js
+++ quesion-10/app.js
@@ -3,7 +3,7 @@ const add = document.querySelectorAll('.add');
 const minus = document.querySelectorAll('.minus');
 const card = document.querySelectorAll('.card');
 const price = document.querySelectorAll('.price');
-const totalPrice = document.getElementById('total-price');
+const totalOrder = document.getElementById('total-price');
 // ................................................................................................
 const state = [
   {
@@ -11,75 +11,74 @@ const state = [
     price: 8000,
     wage: 1000,
     counter: 0,
-    totalPrice: 0,
+    maxPrice: 0,
   },
   {
     id: 2,
     price: 10000,
     wage: 2000,
     counter: 0,
-    totalPrice: 0,
+    maxPrice: 0,
   },
   {
     id: 3,
     price: 10000,
     wage: 3000,
     counter: 0,
-    totalPrice: 0,
+    maxPrice: 0,
   },
   {
     id: 4,
     price: 20000,
     wage: 4000,
     counter: 0,
-    totalPrice: 0,
+    maxPrice: 0,
   },
   {
     id: 5,
     price: 25000,
     wage: 5000,
     counter: 0,
-    totalPrice: 0,
+    maxPrice: 0,
   },
   {
     id: 6,
     price: 10000,
     wage: 6000,
     counter: 0,
-    totalPrice: 0,
+    maxPrice: 0,
   },
   {
     id: 7,
     price: 6000,
     wage: 0,
     counter: 0,
-    totalPrice: 0,
+    maxPrice: 0,
   },
   {
     id: 8,
     price: 5000,
     wage: 0,
     counter: 0,
-    totalPrice: 0,
+    maxPrice: 0,
   },
   {
     id: 9,
     price: 8000,
     wage: 1000,
     counter: 0,
-    totalPrice: 0,
+    maxPrice: 0,
   },
   {
     id: 10,
     price: 25000,
     wage: 3000,
     counter: 0,
-    totalPrice: 0,
+    maxPrice: 0,
   },
 ];
-let arr = [];
 
-// ......................Add Price for each card...................................................
+// ......................Add Price To Each Product...................................................
 price.forEach(elem => {
   for (const item of state) {
     if (item.id === +elem.dataset.id) {
@@ -87,24 +86,30 @@ price.forEach(elem => {
     }
   }
 });
+// ................................Total Order.....................................................
+const toatlOrder = arr => {
+  let total = 0;
+  for (const item of arr) {
+    total += item.maxPrice;
+  }
+  totalOrder.innerHTML = `${total} تومان`;
+};
 // .....................................Add Order..................................................
 const handleAddOrder = e => {
-  let total = 0;
   for (const item of state) {
     if (
       +e.currentTarget.closest('#card-icon').previousElementSibling.dataset
         .id === item.id
     ) {
       item.counter += 1;
-      item.totalPrice = item.price * item.counter;
+      item.maxPrice = item.price * item.counter;
       e.currentTarget.nextElementSibling.innerHTML = item.counter;
       e.currentTarget.parentElement.nextElementSibling.innerHTML = `${
-        item.price * item.counter
+        item.maxPrice
       } تومان`;
-      total += item.price * item.counter;
     }
   }
-  totalPrice.innerHTML = `${total} تومان`;
+  toatlOrder(state);
 };
 // ..................................Remove Order..................................................
 const handleMinusOrder = e => {
@@ -115,15 +120,16 @@ const handleMinusOrder = e => {
           .id === item.id
       ) {
         item.counter -= 1;
-
+        item.maxPrice = item.price * item.counter;
         e.currentTarget.previousElementSibling.innerHTML = item.counter;
         e.currentTarget.parentElement.nextElementSibling.innerHTML = `${
-          item.price * item.counter
+          item.maxPrice
         } تومان`;
       }
     }
   }
+  toatlOrder(state);
 };
-
+// ................................................................................................
 add.forEach(item => item.addEventListener('click', handleAddOrder));
 minus.forEach(item => item.addEventListener('click', handleMinusOrder));
