e6878a1f82f3af4623bef1934d51b7aa87eed90e
Hosein Emdadi <hemdadi71@gmail.com>
2023-02-14 23:11:37 +0330

    question/10 is compeleted

--- quesion-10/app.js
+++ quesion-10/app.js
@@ -4,80 +4,75 @@ const minus = document.querySelectorAll('.minus');
 const card = document.querySelectorAll('.card');
 const price = document.querySelectorAll('.price');
 const totalOrder = document.getElementById('total-price');
+const wagePrice = document.getElementById('wage-price');
+const paymentPrice = document.getElementById('payment');
+const form = document.getElementById('form');
+const discount = document.getElementById('discount-percent');
+const tick = document.getElementById('tick');
+const orderBtn = document.getElementById('orderBtn');
+const container = document.getElementById('container');
+const modal = document.getElementById('modal');
+const modalBody = document.getElementById('modal-body');
+const button = document.getElementById('button');
+const modalText = document.getElementById('modalText');
 // ................................................................................................
 const state = [
   {
     id: 1,
     price: 8000,
     wage: 1000,
-    counter: 0,
-    maxPrice: 0,
   },
   {
     id: 2,
     price: 10000,
     wage: 2000,
-    counter: 0,
-    maxPrice: 0,
   },
   {
     id: 3,
     price: 10000,
     wage: 3000,
-    counter: 0,
-    maxPrice: 0,
   },
   {
     id: 4,
     price: 20000,
     wage: 4000,
-    counter: 0,
-    maxPrice: 0,
   },
   {
     id: 5,
     price: 25000,
     wage: 5000,
-    counter: 0,
-    maxPrice: 0,
   },
   {
     id: 6,
     price: 10000,
     wage: 6000,
-    counter: 0,
-    maxPrice: 0,
   },
   {
     id: 7,
     price: 6000,
     wage: 0,
-    counter: 0,
-    maxPrice: 0,
   },
   {
     id: 8,
     price: 5000,
     wage: 0,
-    counter: 0,
-    maxPrice: 0,
   },
   {
     id: 9,
     price: 8000,
     wage: 1000,
-    counter: 0,
-    maxPrice: 0,
   },
   {
     id: 10,
     price: 25000,
     wage: 3000,
-    counter: 0,
-    maxPrice: 0,
   },
 ];
-
+for (const item of state) {
+  item.counter = 0;
+  item.maxPrice = 0;
+  item.maxWage = 0;
+}
 // ......................Add Price To Each Product...................................................
 price.forEach(elem => {
   for (const item of state) {
@@ -89,10 +84,14 @@ price.forEach(elem => {
 // ................................Total Order.....................................................
 const toatlOrder = arr => {
   let total = 0;
+  let wage = 0;
   for (const item of arr) {
     total += item.maxPrice;
+    wage += item.maxWage;
   }
   totalOrder.innerHTML = `${total} تومان`;
+  wagePrice.innerHTML = `${wage} تومان`;
+  paymentPrice.innerHTML = `${total + wage} تومان`;
 };
 // .....................................Add Order..................................................
 const handleAddOrder = e => {
@@ -103,10 +102,9 @@ const handleAddOrder = e => {
     ) {
       item.counter += 1;
       item.maxPrice = item.price * item.counter;
+      item.maxWage = item.wage * item.counter;
       e.currentTarget.nextElementSibling.innerHTML = item.counter;
-      e.currentTarget.parentElement.nextElementSibling.innerHTML = `${
-        item.maxPrice
-      } تومان`;
+      e.currentTarget.parentElement.nextElementSibling.innerHTML = `${item.maxPrice} تومان`;
     }
   }
   toatlOrder(state);
@@ -121,15 +119,58 @@ const handleMinusOrder = e => {
       ) {
         item.counter -= 1;
         item.maxPrice = item.price * item.counter;
+        item.maxWage = item.wage * item.counter;
         e.currentTarget.previousElementSibling.innerHTML = item.counter;
-        e.currentTarget.parentElement.nextElementSibling.innerHTML = `${
-          item.maxPrice
-        } تومان`;
+        e.currentTarget.parentElement.nextElementSibling.innerHTML = `${item.maxPrice} تومان`;
       }
     }
   }
   toatlOrder(state);
 };
+// .......................................Payment Price............................................
+const handlePaymentPrice = e => {
+  e.preventDefault();
+  let newPeyment = +paymentPrice.innerHTML.split('تومان')[0];
+  const input =
+    e.currentTarget.parentElement.previousElementSibling.value.toLowerCase();
+  if (input === 'golden') {
+    discount.innerHTML = `${30} درصد`;
+    paymentPrice.innerHTML = `${newPeyment - newPeyment * 0.3} تومان`;
+  } else if (input === 'silver') {
+    discount.innerHTML = `${20} درصد`;
+    paymentPrice.innerHTML = `${newPeyment - newPeyment * 0.2} تومان`;
+  } else if (input === 'bronze') {
+    discount.innerHTML = `${10} درصد`;
+    paymentPrice.innerHTML = `${newPeyment - newPeyment * 0.1} تومان`;
+  }
+  form.reset();
+};
+// ........................................Show Modal................................................
+const handleShowModal = () => {
+  if (paymentPrice.innerHTML === '0 تومان') {
+    modalText.innerHTML = `شما چیزی سفارش نداده اید.`;
+    button.innerHTML = `بازگشت`;
+  } else {
+    modalText.innerHTML = `سفارش شما ثبت شد.`;
+    button.innerHTML = `باشه`;
+  }
+  container.classList.add('blur');
+  modal.classList.remove('d-none');
+  modal.classList.add('d-block');
+  modalBody.classList.remove('d-none');
+  modalBody.classList.add('d-block');
+};
+// .......................................Hide Modal..................................................
+const handleHideModal = () => {
+  container.classList.remove('blur');
+  modal.classList.add('d-none');
+  modal.classList.remove('d-block');
+  modalBody.classList.add('d-none');
+  modalBody.classList.remove('d-block');
+};
 // ................................................................................................
 add.forEach(item => item.addEventListener('click', handleAddOrder));
 minus.forEach(item => item.addEventListener('click', handleMinusOrder));
+tick.addEventListener('click', handlePaymentPrice);
+orderBtn.addEventListener('click', handleShowModal);
+button.addEventListener('click', handleHideModal);
