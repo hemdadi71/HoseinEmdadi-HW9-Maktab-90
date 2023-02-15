9b2c1a957c611303c58e5713449955045d408e83
Hosein Emdadi <hemdadi71@gmail.com>
2023-02-15 19:41:40 +0330

    question/8 compeleted

--- question-8/app.js
+++ question-8/app.js
@@ -2,20 +2,20 @@
 
 const item = document.querySelectorAll('.item');
 const alertArr = Array.from(alert);
-
+// ............................................................
 const imgAttr = { src: 'img/close.png', width: '25px' };
-
+//....................................................
 function setAttributes(el, attrs) {
   for (var key in attrs) {
     el.setAttribute(key, attrs[key]);
   }
 }
-
+// ...................................................
 const closeAlert = e => {
   e.currentTarget.parentElement.style.opacity = '0';
   e.currentTarget.parentElement.style.transition = 'all 1s';
 };
-
+// ..............................................................
 item.forEach(element => {
   const img = document.createElement('img');
   setAttributes(img, imgAttr);
