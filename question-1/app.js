2002ce58ab4ff1405bdf6f52ecdd558bdce354c7
Hosein Emdadi <hemdadi71@gmail.com>
2023-02-16 08:58:20 +0330

    question compeleted

--- question-1/app.js
+++ question-1/app.js
@@ -2,21 +2,13 @@
 
 const table = document.getElementById('age-table');
 console.log(table.outerHTML);
-
 const labels = table.querySelectorAll('label');
 console.log(labels);
-
-const td = table.childNodes[1].children[0].children[0];
+const td = table.querySelector('td');
 console.log(td.outerHTML);
-
-const formSearch_1 = document.getElementsByName('search')[0];
-console.log(formSearch_1.outerHTML);
-
-const FormSearch = document.querySelectorAll('form')[0];
-console.log(FormSearch.outerHTML);
-
-const firstInput = FormSearch.children[0].children[0];
+const formSearch = document.querySelector('form[name="search"]');
+console.log(formSearch.outerHTML);
+const firstInput = formSearch.querySelector('input');
 console.log(firstInput.outerHTML);
-
-const lastInput = FormSearch.children[1];
+const lastInput = formSearch.querySelector('input:nth-child(2)');
 console.log(lastInput.outerHTML);
